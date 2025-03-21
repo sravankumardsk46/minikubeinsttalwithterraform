126156dfd111b9d17d3138ba4d73dadb709e915a
sravankumardsk46 <sravankumardsk7@gmail.com>
2025-03-21 16:39:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,10 +6,6 @@ variable "instance_type" {
   default = "t2.medium"
 }
 
-variable "key_name" {
-  default = "my-key"  # Change to your key pair name
-}
-
 variable "ami_id" {
   default = "ami-0c55b159cbfafe1f0"  # Ubuntu 20.04 (Change based on region)
 }
